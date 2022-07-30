b121175fffa585f8640b322866a7233284aa18f9
hpmmg <annenkovdaniil.l@gmail.com>
2022-07-30 13:47:41 +0300

    added section "top"

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,11 @@ $(function () {
 
   $('.top-slider__inner').slick({
     dots: true,
-    arrows: true,
-    fade: true,
+    // fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/topslider/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/topslider/arrow-right.svg" alt=""></button>',
   });
 
 
