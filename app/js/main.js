337611712de12553b255deb7e4b2e0cc5ba4ab6d
hpmmg <annenkovdaniil.l@gmail.com>
2022-08-23 17:51:42 +0300

    added adaptive section "top" & burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,24 @@
 $(function () {
 
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header').toggleClass('header--active')
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     // fade: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/topslider/arrow-left.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/topslider/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 730,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
 
 
@@ -21,9 +33,9 @@ $(function () {
   $('.menu__list-link').on('click', function () {
     $('.menu__list-link').removeClass('menu__list-link--active')
   });
-  $('.menu__list-link').on('click', function () {
-    $(this).addClass('menu__list-link--active')
-  });
+  // $('.menu__list-link').on('click', function () {
+  //   $(this).addClass('menu__list-link--active')
+  // });
 
   var swiper = new Swiper(".blog__slider", {
     
